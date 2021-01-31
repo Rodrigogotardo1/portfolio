854b5cbc805f47476754b0b71175be03659d1abd
francisco-vieira <francisco.vieira.dev@gmail.com>
2021-01-31 09:47:19 -0300

    modificação da tela home

--- app/model/connection/Connection.php
+++ app/model/connection/Connection.php
@@ -7,14 +7,14 @@ class Connection
 
     public static function getConnection(): PDO
     {
-        $dns = 'mysql:dbname=sitedb; host=localhost;charset=utf8;';
-        $user = 'francisco';
-        $password = '!@adminDB1';
+//        $dns = 'mysql:dbname=sitedb; host=localhost;charset=utf8;';
+//        $user = 'francisco';
+//        $password = '!@adminDB1';
 
 
-//        $dns = 'mysql:dbname=epiz_26679661_sitedb;host=sql312.epizy.com;port=3306;charset=utf8;';
-//        $user = 'epiz_26679661';
-//        $password = 'wQ288gISROz';
+        $dns = 'mysql:dbname=epiz_26679661_sitedb;host=sql312.epizy.com;port=3306;charset=utf8;';
+        $user = 'epiz_26679661';
+        $password = 'wQ288gISROz';
 
         try {
             if (self::$conn == null) {
